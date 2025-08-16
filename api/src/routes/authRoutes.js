a3563b8c3211998231d7f9bcf52ecf5830c6ccdd
Pexe <davidhenriqusms18@gmail.com>
2025-08-15 20:48:25 -0400

    feat: limitar acessos em rotas sensíveis

--- api/src/routes/authRoutes.js
+++ api/src/routes/authRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const { createUserController, loginController, refreshController, logoutController } = require('../controllers/authController');
 const { validate } = require('../middlewares/validate');
-const { loginRateLimiter } = require('../middlewares/rateLimiter');
+const { authRateLimiter } = require('../middlewares/rateLimiter');
 const { authMiddleware } = require('../middlewares/auth');
 const { createUserSchema, loginSchema, refreshTokenSchema } = require('../validators/authSchemas');
 
 const router = express.Router();
 
-router.post('/create-user', validate(createUserSchema), createUserController);
-router.post('/login', loginRateLimiter, validate(loginSchema), loginController);
+router.post('/create-user', authRateLimiter, validate(createUserSchema), createUserController);
+router.post('/login', authRateLimiter, validate(loginSchema), loginController);
 router.post('/refresh-token', validate(refreshTokenSchema), refreshController);
 router.post('/logout', authMiddleware, logoutController);
 
